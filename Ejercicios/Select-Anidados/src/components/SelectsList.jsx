eb39396961d9f37d3481cc41f06795e5f75501bc
Eli0024 <eliana199972@gmail.com>
2024-09-04 21:54:25 -0500

    modificado

--- Ejercicios/Select-Anidados/src/components/SelectsList.jsx
+++ Ejercicios/Select-Anidados/src/components/SelectsList.jsx
@@ -5,16 +5,16 @@ export const SelectList = ({ manejadorCambio, title, url, data, value }) => {
   const key = `select-${title}`;
   const label = title.toUpperCase();
 
-  // Usar el hook useFetch para obtener los datos si no se pasan como props
+
   const { data: fetchedData, error, loading } = useFetch(url);
 
   if (loading) return <p>Cargando...</p>;
-  if (error) return <p>Ha ocurrido un error: {error.message}</p>;
+ 
 
-  // Usar data si se pasa, de lo contrario usar fetchedData
+
   const options = data || fetchedData || [];
 
-  return (
+  return ( 
     <div className="container-select">
       <label className="label-select" htmlFor={key}>
         {label}
---
+++ validate-forms/src/components/ContactForm.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import { useForms } from "../hooks/useForms";
+import { Loader } from './Loader';
+import Mail from './Mail';
+
+//Valores por defecto para el formulario
+const valorDefecto = {
+  nombre: "",
+  email: "",
+  asunto: "",
+  observaciones: "",
+};
+
+//Validaciones para verificar el contenido y que tengan el formato correcto
+const validaciones = (form) => {
+  //Puede tener cualquier caracter alfanumerico + @ + cualquier letra + . + cualquier caracter minimo 2 maximo 6
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,6}$/;
+  //Puede tener minimo 1 y maximo 255 caracteres
+  let regexComments = /^.{1,255}$/;
+
+  let errorsFormulario = {};
+
+  //Validaciones de inputs vacios y sin el patron especificado
+  if (!form.nombre.trim()) {
+    errorsFormulario.nombre = "El campo es obligatorio";
+  }
+  if (!form.email.trim()) {
+    errorsFormulario.email = "El campo es obligatorio";
+  } else if (!regexEmail.test(form.email.trim())) {
+    errorsFormulario.email =
+      "El campo no tiene el formato de un correo electronico";
+  }
+  if (!form.asunto.trim()) {
+    errorsFormulario.asunto = "El campo es obligatorio";
+  }
+  if (!form.observaciones.trim()) {
+    errorsFormulario.observaciones = "El campo es obligatorio";
+  } else if (!regexComments.test(form.observaciones.trim())) {
+    errorsFormulario.observaciones =
+      "El campo debe tener minimo 1 caracter y maximo 255 caracteres";
+  }
+
+  return errorsFormulario;
+};
+
+const ContactForm = () => {
+  //Llamado a nuestro custom hook recibiendo mis variables y mis funciones
+  const { form, errores, cargando, bd, respuesta, manejadorCambios, manejadorSalidaInput, enviarFormulario } = useForms(
+    valorDefecto,
+    validaciones
+  );
+  
+  return (
+    <>
+    <div div className="contact-form-container">
+      <h1>Formulario de contacto</h1>
+      {/* con el evento onSubmit ejecutamos una funcion cuando se envia el formulario */}
+      <form onSubmit={enviarFormulario} className="contact-form">
+        {/* Nombre */}
+        <div className="form-group">
+        <label htmlFor="nombre">Nombre</label>
+        <input
+          type="text"
+          name="nombre"
+          placeholder="Escribe tu nombre..."
+          // required
+          value={form.nombre}
+          onChange={manejadorCambios}
+          onBlur={manejadorSalidaInput}
+        />
+        {/* Muestra la etiqueta p con el error solo si tiene un error en nombre */}
+        {errores.nombre && <p className="p-error">{errores.nombre}</p>}
+        </div>
+        {/* Email */}
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Escribe tu correo electronico..."
+          // required
+          value={form.email}
+          onChange={manejadorCambios}
+          onBlur={manejadorSalidaInput}
+        />
+        {/* Muestra la etiqueta p con el error solo si tiene un error en email */}
+        {errores.email && <p className="p-error">{errores.email}</p>}
+        </div>
+        {/* Asunto */}
+        <div className="form-group">
+          <label htmlFor="asunto">Asunto</label>
+        <input
+          type="text"
+          name="asunto"
+          placeholder="Asunto..."
+          // required
+          value={form.asunto}
+          onChange={manejadorCambios}
+          onBlur={manejadorSalidaInput}
+        />
+        {/* Muestra la etiqueta p con el error solo si tiene un error en asunto */}
+        {errores.asunto && <p className="p-error">{errores.asunto}</p>}
+        </div>
+        {/* Observaciones */}
+        <div className="form-group">
+        <label htmlFor="observaciones">Observaciones</label>
+        <textarea
+          name="observaciones"
+          placeholder="Escribe tus observaciones..."
+          // required
+          cols={50}
+          rows={5}
+          value={form.observaciones}
+          onChange={manejadorCambios}
+          onBlur={manejadorSalidaInput}
+        />
+        {/* Muestra la etiqueta p con el error solo si tiene un error en observaciones */}
+        {errores.observaciones && (
+          <p className="p-error">{errores.observaciones}</p>
+        )}
+        </div>
+        {/* Button que envia el formulario */}
+        <input type="submit" value="Enviar" disabled={cargando} className={cargando ? 'deshabilitado' : ''}/>
+      </form>
+      {/* Si cargando es True renderiza <Loader/> */}
+      {cargando && <Loader/>}
+      {respuesta && <Mail datos={bd}/>}
+      </div>
+    </>
+  );
+};
+
+export default ContactForm;
